211aba95c336978172e1c4595224890725caea1e
Nick <nicholasiapalucci@outlook.com>
2022-11-03 21:39:28 -0400

    Initial commit

---
+++ data_generation/college_data_generator.py
@@ -0,0 +1,125 @@
+from bs4 import BeautifulSoup
+import json
+from urllib.request import urlopen
+import re as regex
+
+ug_json = {}
+
+def html(url):
+    return BeautifulSoup(urlopen(url).read(), features = "html.parser")
+
+# "PHYS 182 [Min Grade: C-] and (MATH 102 [Min Grade: C-] or MATH 183 [Min Grade: C-])"
+# "PHYS-182 [Min Grade: C-] and (MATH-102 [Min Grade: C-] or MATH-183 [Min Grade: C-])"
+def parse_prereqs(prereq_string):
+    prereq_string = regex.sub(r"([A-Z])\s(\d+)", r"\1-\2", prereq_string)
+
+    # Tokenize
+    token_types = {
+        "class": r"^[A-Z]+\-\d+",
+        "whitespace": r"^\s+",
+        "grade": r"^\[Min Grade: (.+?)\]",
+        "and": r"^and\b",
+        "or": r"^or\b",
+        "left parentheses": r"^\(",
+        "right parentheses": r"^\)"
+    }
+    tokens = []
+    remaining_string = prereq_string
+    while(remaining_string):
+        match_found = False
+        for type_, regexp in token_types.items():
+            match = regex.match(regexp, remaining_string)
+            if match:
+                if type_ == "grade": tokens.append({ "type": type_, "value": match.group(1) })
+                elif type_ != "whitespace": tokens.append({ "type": type_, "value": match.group(0) })
+                remaining_string = remaining_string[len(match.group(0)):]
+                match_found = True
+                break
+        if not match_found: return [remaining_string]
+
+    # Parser
+
+    def next(type_):
+        if len(tokens_to_parse) == 0: return False
+        if type_ and tokens_to_parse[0]["type"] != type_: raise Exception("Expected type " + type_ + " but found " + tokens_to_parse[0]['type'] + " in\n" + "\n".join(map(lambda token : str(token), tokens)))
+        return tokens_to_parse.pop(0)
+
+    def next_is(type_):
+        return tokens_to_parse[0]["type"] == type_ if len(tokens_to_parse) > 0 else False
+
+    def parse_and_or(left):
+        while next_is("and"):
+            next("and")
+            expr = parse_expr()
+            if type(expr) is list: left = [left, *expr]
+            else: left = [left, expr]
+        while next_is("or"):
+            next("or")
+            expr = parse_expr()
+            if "one of" in expr: left = { "one of": [left, *expr.get("one of")] }
+            else: left = { "one of": [left, expr] }
+        return left
+
+    def parse_expr():
+        if next_is("left parentheses"):
+            next("left parentheses")
+            expr = parse_expr()
+            next("right parentheses")
+            if (next_is("and") or next_is("or")): return parse_and_or(expr)
+            return expr
+        if next_is("class"):
+            course_name = next("class")["value"]
+            grade = "Any"
+            if next_is("grade"): grade = next("grade")["value"]
+            return parse_and_or({
+                "course name": course_name,
+                "minimum grade": grade
+            })
+        raise Exception("Unexpected token: " + str(tokens_to_parse[0]))
+
+    prerequisites = []
+    tokens_to_parse = tokens.copy()
+
+    while next_is("or") or next_is("and"):
+        next(None)
+
+    while(tokens_to_parse):
+        prerequisites.append(parse_expr())
+
+    return prerequisites
+
+i = 0
+ugSoup = html("https://catalog.drexel.edu/coursedescriptions/quarter/undergrad")
+for element in ugSoup.find_all("a"):
+    text_contents = element.decode_contents()
+    if "(" in text_contents and ")" in text_contents:
+        href = element.get("href")
+        major_soup = html("https://catalog.drexel.edu" + href)
+        major_name = text_contents[:text_contents.index("(") - 1].replace("&amp;", "&")
+        print("Getting stats for " + major_name)
+
+        for course_block in major_soup.find_all("div", class_="courseblock"):
+            title = course_block.find_all("span", class_="cdspacing")
+            code = regex.sub(r"[^\w]", "-", title[0].decode_contents()[:-2])
+            name = title[1].decode_contents().replace("&amp;", "&")
+            credits = title[2].previous_sibling.strip()
+            if "-" in credits:
+                credits = credits[credits.index("-") + 1:].strip()
+
+            prereqs = course_block.find_all("b")[-1]
+            prereq_list = []
+            if prereqs and not "credit" in prereqs.next_sibling:
+                prereq_list = parse_prereqs(prereqs.next_sibling.strip())
+
+            college = course_block.find("b").next_sibling.strip()
+            if not ug_json.get(college): ug_json[college] = {}
+            
+            if not ug_json[college].get(major_name): ug_json[college][major_name] = {}
+            ug_json[college][major_name][code] = {
+                "course name": name,
+                "credits": credits,
+                "prerequisites": prereq_list if prereq_list else []
+            }
+        i += 1
+
+open("data/colleges.json", "w").write(json.dumps(ug_json, indent=4))
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import * as colleges from "../data/colleges.json";
+
+export { colleges };
